9999aba2f4ac7cc513cf4dfdeae33929b6728b7a
Angel Pumacayo <angelpumacayo@yahoocom>
2017-02-09 20:33:50 -0800

    Add books

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Book.destroy_all
+
+
+
+Book.create!([{
+	title: "Hyperion",
+	author: "Dan Simmons",
+	description: "Probably the best science book i've ever read!! Love it!. I read the book three times now and everytime i read it i always tell my friends how awesome the book is, i always recommend it.",
+	amazon_id: "0553283685",
+  rating: 5,
+  finished_on: 10.days.ago
+	},
+	{
+	title: "Jony I've: The Genius behind Apple's Greatest Products",
+	author: "Leander Kahney",
+	description: "Even though I respect I've, I felt this biography only hit skin deep. It wen all over the major events in his life, his passion for design, awards he achieved -- but that's really it. I dont feel i know him anymore than before reading this.",
+	amazon_id: "159184617x",
+  rating: 4,
+  finished_on: 1.day.ago
+	},
+	{
+  title: "Mindstorms",
+	author: "Seymour A. Papert",
+	description: "Although this book focuses on the cognitive advantages to having children use technology from an early age, it is also a in depth look at how people can learn from themselves. As someone who was often distracted and bored at times during school, Mindstorms highlights some of the reasoning behind that feeling and what we can do as teachers to help minimize it.",
+	amazon_id: "0465046746",
+  rating: 1,
+  finished_on: nil
+	}])
+
+p "Created #{Book.count} books"
